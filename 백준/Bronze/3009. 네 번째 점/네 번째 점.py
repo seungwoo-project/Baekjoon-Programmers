d67abdb85bc98d2556a4e6b5651d7f280fc48cc8
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2023-03-27 20:12:32 +0900

    [Bronze III] Title: 네 번째 점, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,15 @@
+xp=[]
+yp=[]
+
+for _ in range(3):
+    x,y=map(int,input().split())
+    xp.append(x)
+    yp.append(y)
+
+for i in range(3):
+    if xp.count(xp[i])==1:
+        x4=xp[i]
+    if yp.count(yp[i])==1:
+        y4=yp[i]
+
+print(x4,y4)
