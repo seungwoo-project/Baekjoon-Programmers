d6e0a6442b443ca106eaf9c5330b55201e138258
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2023-07-25 15:19:57 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,25 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[9] , sum =0;
-pair<int,int> ret;
-
+int a[9];
+void solve() {
+	int sum=0;
+	for(int i = 0 ; i < 7 ; i++) {
+		sum += a[i];
+	}
+	
+	if (sum == 100) {
+		for(int i = 0 ; i < 7 ; i++) {
+			cout << a[i] << '\n';
+		}
+		exit(0);
+	}
+	return ;
+}
 int main() {
 	for(int i = 0 ; i < 9 ; i++) {
 		cin >> a[i];
-		sum += a[i];
 	}
 	sort(a,a+9);
-	for(int i = 0 ; i < 9 ; i++) {
-		for(int j = i+1 ; j < 9 ; j++) {
-			if(sum-a[i]-a[j]==100) {
-				ret = {i,j};
-			}
-		}
-	}
-	
-	for(int i = 0 ; i < 9 ; i++) {
-		if(ret.first == i || ret.second == i) continue;
-		cout << a[i] << '\n';
-	}
+	do{
+		solve();
+	} while(next_permutation(a,a+9));
 	return 0;
 }
