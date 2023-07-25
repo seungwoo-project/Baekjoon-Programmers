c6ec42e41ba644dfa06e134995d45a27942b69c5
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2023-07-25 21:51:24 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int al[26];
+string a;
+int main() {
+	cin >> a;
+	for(char c : a) {
+		al[c-97] += 1;
+	}
+	
+	for(int i : al) cout << i << ' ';
+	return 0;
+}
+
+// 알파벳의 개수가 2개일때 는
+ 
