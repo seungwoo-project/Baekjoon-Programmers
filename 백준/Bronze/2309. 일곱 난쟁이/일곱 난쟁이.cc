54c1bb3b7e118c7a53ab32f4c09fd3e182ee0c07
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2023-07-25 15:11:40 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,37 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a;
-vector<int> v;
-int n =9;
-bool p = false;
+int a[9] , sum =0;
+pair<int,int> ret;
+
 int main() {
-	for(int i = 0 ; i < n ; i++) {
-		cin >> a; // 9명 키를 다 입력받음
-		v.push_back(a);
+	for(int i = 0 ; i < 9 ; i++) {
+		cin >> a[i];
+		sum += a[i];
 	}
-	
-	int sum = accumulate(v.begin(),v.end(),0);
-	
-	for(int i = 0 ; i < n ; i++){
-		for(int j = i+1 ; j < n ; j++) {
-			if((sum-(v[i]+v[j]))==100){
-				if(i<j){
-					v.erase(v.begin()+j);
-					v.erase(v.begin()+i);
-				} else {
-					v.erase(v.begin()+i);
-					v.erase(v.begin()+j);
-				}
-				p=true;
-				break;
+	sort(a,a+9);
+	for(int i = 0 ; i < 9 ; i++) {
+		for(int j = i+1 ; j < 9 ; j++) {
+			if(sum-a[i]-a[j]==100) {
+				ret = {i,j};
 			}
 		}
-		if(p) break;
 	}
-
 	
-	
-	sort(v.begin(),v.end());
-	for(int i : v) cout << i << '\n';
+	for(int i = 0 ; i < 9 ; i++) {
+		if(ret.first == i || ret.second == i) continue;
+		cout << a[i] << '\n';
+	}
 	return 0;
 }
