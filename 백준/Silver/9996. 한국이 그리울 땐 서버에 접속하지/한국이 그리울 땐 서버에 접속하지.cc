f38ea1a3fdaef798abe5f884b68a5dfe5e04b0f9
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2023-07-30 15:40:09 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+string key, m, s, e, a;
+int pos;
+vector<string> ret;
+int main() {
+	cin >> n;
+	cin >> key;
+	pos = key.find("*");
+	s=key.substr(0,pos);
+	e=key.substr(pos+1,key.length());
+	a=s+e;
+	for(int i = 0 ; i < n ; i++) {
+		cin >> m;
+		if((m.length()>=a.length()) && (s==m.substr(0,s.length())) && (e==m.substr(m.length()-e.length(),m.length()))) ret.push_back("DA");
+		else ret.push_back("NE");
+	}
+	
+	for(string i : ret) cout << i << '\n';
+	return 0;
+}
