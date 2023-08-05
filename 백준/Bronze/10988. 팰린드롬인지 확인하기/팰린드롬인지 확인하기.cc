81f937dcc280688cbf707680a2b4b36b22043b23
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2023-08-05 19:12:30 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,16 +1,11 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-string s;
-string rs;
+string s,temp;
 int main() {
 	cin >> s;
-	for(int i =  s.length()-1 ; i>=0 ; i--) {
-		rs+=s[i];
-	}
-	
-	if(s==rs) cout << '1';
+	temp=s;
+	reverse(s.begin(),s.end());
+	if(s==temp) cout << '1';
 	else cout << '0';
-	
 	return 0;
 }
