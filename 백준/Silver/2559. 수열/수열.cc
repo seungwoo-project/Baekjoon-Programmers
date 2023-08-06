df356bd9b73356bb15b4824f6f291d1e08a88be2
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2023-08-06 18:15:37 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,32 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n,k;
-int x[100004];
-int psum[100004];
-vector<int> ret;
-// n-k
-// accumulate
-// 시간초과  
-
+int n, k;
+int a[100004], psum[100004];
+int max_n=-1e9;
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 	cin >> n >> k;
 	for(int i = 1 ; i <= n ; i++) {
-		cin >> x[i];
-		psum[i]=psum[i-1]+x[i];
+		cin >> a[i];
+	}
+	
+	for(int i = 1 ; i <= n ; i++) {
+		psum[i] = psum[i-1] + a[i];
 	}
 	
 	for(int i = k ; i <= n ; i++) {
-		ret.push_back(psum[i]-psum[i-k]);
+		max_n=max(max_n,psum[i] - psum[i-k]);
 	}
-	
-	cout << *max_element(ret.begin(),ret.end());
+	cout << max_n;
 	return 0;
 }
-
-
-// 누적합 psum
-
- 
