32a1670c8c4356929b97c9527610c8805becda90
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2023-09-26 23:26:37 +0900

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -1,63 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int n, ret = -2e9, a, b;
-char c, op;
-vector<int> vn, v; vector<char> vo;
-
-bool pass(int i){
-    for(auto it : v)
-        if(it == i) return true;
-    return false;
+int n,x,y,ret = -2e9;
+char opp;
+char c;
+vector<int> b;
+vector<int> v;
+vector<char> op;
+int opr(int x,int y,char pp) {
+	if(pp == '+') return x+y;
+	if(pp == '-') return x-y;
+	if(pp == '*') return x*y;
+} 
+void go(vector<int> v) {
+	for(int i = b.size()-1 ; i >= 0 ; i--) {
+		int idx = b[i];
+		x = v[idx];	y = v[idx+1]; opp = op[idx];
+		v.erase(v.begin()+idx,v.begin()+idx+2);
+		v.insert(v.begin()+idx,opr(x,y,opp));
+	}
+	
+	for(int i = 0 ; i < op.size() ; i++) {
+		if(find(b.begin(),b.end(),i) != b.end()) continue;
+		x = v[0];	y = v[1]; opp = op[i];
+		v.erase(v.begin(),v.begin()+2);
+		v.insert(v.begin(),opr(x,y,opp));
+	}
+	ret = max(ret,v[0]);
 }
-
-int opr(int a, int b, char op){
-    if(op == '+') return a + b;
-    if(op == '-') return a - b;
-    if(op == '*') return a * b;
+// 연산자의 조합을 뽑아 연산해보자 
+void combi(int start,int k) {
+	if(b.size() == k) {
+		go(v);
+	}
+	
+	for(int i = start + 2 ; i < op.size() ; i++) {
+		b.push_back(i);
+		combi(i,k);
+		b.pop_back();
+	}
+	
 }
-
-void go(vector<int> vn){
-    for(int i = v.size() - 1; i >= 0; i--){
-        int idx = v[i];
-        auto it = vn.begin();
-        op = vo[idx]; a = vn[idx]; b = vn[idx + 1];
-        vn.erase(it + idx, it + idx + 2);
-        vn.insert(it + idx, opr(a, b, op));
-    }
-    for(int i = 0; i < vo.size(); i++){
-        if(pass(i)) continue;
-        auto it = vn.begin();
-        op = vo[i]; a = vn[0]; b = vn[1];
-        vn.erase(it, it + 2);
-        vn.insert(it, opr(a, b, op));
-    }
-    ret = max(ret, vn[0]);
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n;
+	for(int i = 0 ; i < n ; i++) {
+		cin >> c; 
+		if(i % 2 == 0) v.push_back(c - '0'); // 피연산자 
+		else op.push_back(c); // 연산자 
+		
+	}
+	for(int i = 1 ; i < n && i <= 5 ; i++) {
+		combi(-2,i);	
+	} 
+	if(n == 1) cout << v[0];
+	else cout << ret;
+	return 0;
 }
-
-void combi(int s, int r){
-    if(v.size() == r){
-        go(vn);
-    }
-    for(int i = s + 2; i < vo.size(); i++){
-        v.push_back(i);
-        combi(i, r);
-        v.pop_back();
-    }
-}
-
-int main(){
-    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> c;
-        if(i % 2 == 0) vn.push_back(c - '0');
-        else vo.push_back(c);
-    }
-    for(int i = 1; i < n && i <= 5; i++){
-        combi(-2, i);
-    }
-    if(n == 1) cout << vn[0] << "\n";
-    else cout << ret << "\n";
-    return 0;
-}
+//9
+//3+8*7-9*2
