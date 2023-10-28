191c264d168bc3f97a7b58784d7f40b1af779bc6
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2023-10-28 16:59:19 +0900

    [Platinum V] Title: 백조의 호수, Time: 256 ms, Memory: 29904 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.cc
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.cc
@@ -1,47 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-char a[1504][1504];
-queue<pair<int,int>> water, watertemp, swan, swantemp;
-int cnt, swany, swanx, x, y, r, c;
 const int dy[] = {-1,0,1,0}, dx[] = {0,1,0,-1};
-int visited[1504][1504] , visited_swan[1504][1504];
-bool swan_move() {
-	while(swan.size()) {
-		tie(y,x) = swan.front();
-		swan.pop();
+int x,y,r,c, water_visited[1504][1504], back_visited[1504][1504], day, backX,backY;
+char a[1504][1504];
+queue<pair<int,int>> water, back, watertemp, backtemp;
+void qclear(queue<pair<int,int>> &q) {
+	queue<pair<int,int>> empty;
+	swap(q,empty);
+}
+bool move_back() {
+	while(back.size()) {
+		tie(y,x) = back.front();
+		back.pop();
 		for(int i = 0 ; i < 4 ; i++) {
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			if(ny < 0 || ny >= r || nx < 0 || nx >= c || visited_swan[ny][nx]) continue;
-			visited_swan[ny][nx] = 1;
-			if(a[ny][nx] == 'X') swantemp.push({ny,nx});
-			else if (a[ny][nx] == '.') swan.push({ny,nx});
-			else if(a[ny][nx] == 'L') return true;
+			if(ny < 0 || ny >= r || nx < 0 || nx >= c || back_visited[ny][nx]) continue;
+			if(a[ny][nx] == 'L') return true;
+			if(a[ny][nx] == 'X') {
+				back_visited[ny][nx] = 1;
+				backtemp.push({ny,nx});
+			} 	
+			if(a[ny][nx] == '.') {
+				back_visited[ny][nx] = 1;
+				back.push({ny,nx}); 
+			}
 		}
 	}
 	return false;
 }
 
-void water_move() {
+void water_melting() {
 	while(water.size()) {
 		tie(y,x) = water.front();
 		water.pop();
 		for(int i = 0 ; i < 4 ; i++) {
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			if(ny < 0 || ny >= r || nx < 0 || nx >=c || visited[ny][nx]) continue;
+			if(ny < 0 || ny >= r || nx < 0 || nx >= c || water_visited[ny][nx]) continue;
 			if(a[ny][nx] == 'X') {
-				visited[ny][nx] = 1;
-				a[ny][nx] = '.';
+				water_visited[ny][nx] = 1;
 				watertemp.push({ny,nx});
-			}
+				a[ny][nx] = '.';
+			} 	
 		}
 	}
-}
-
-void qclear(queue<pair<int,int>> &q) {
-	queue<pair<int,int>> empty;
-	swap(q,empty);
 }
 int main() {
 	cin >> r >> c;
@@ -49,27 +52,27 @@ int main() {
 		for(int j = 0 ; j < c ; j++) {
 			cin >> a[i][j];
 			if(a[i][j] == 'L') {
-				swany = i;
-				swanx = j;
+				backX = j;
+				backY = i;
 			}
 			if(a[i][j] == '.' || a[i][j] == 'L') {
-				visited[i][j] = 1;
+				water_visited[i][j] = 1;
 				water.push({i,j});
 			}
-		}
+		}	
 	}
 	
-	swan.push({swany, swanx});
-	visited_swan[swany][swanx] = 1;
+	back_visited[backY][backX] = 1;
+	back.push({backY,backX});
 	while(1) {
-		if(swan_move()) break;
-		water_move();
+		if(move_back()) break;
+		water_melting();
+		back = backtemp;
 		water = watertemp;
-		swan = swantemp;
+		qclear(backtemp);
 		qclear(watertemp);
-		qclear(swantemp);
-		cnt++;
+		day++;
 	}
-	cout << cnt;
+	cout << day;
 	return 0;
 }
