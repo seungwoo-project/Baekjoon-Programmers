4e331fd46ab166c12ff12f1cbecfad319a1363e5
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2023-10-29 21:48:21 +0900

    [Silver I] Title: 완전 이진 트리, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
@@ -1,30 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-int k, a[1029];
-vector<int> v[1029];
-void go(int s, int e , int level) {
-	if(s > e) return ;
-	if(s==e) {
-		v[level].push_back(a[s]);
+int n, a[1030];
+vector<int> v[11]; 
+void go(int s ,int e , int level) {
+	if(s > e) return ; // s가 e보다 높을 순 없으니 안됨
+	if(s == e) {
+		v[level].push_back(a[s]); // 앞 과 끝이 같다면 mid는 앞과 뒤 의 값이 됨 값 푸쉬 
 		return ;
-	}
+	} 
 	
-	int mid = (s + e) / 2;
+	int mid = (s+e) / 2; // 중간값을 레벨별로의 벡터에 넣음
 	v[level].push_back(a[mid]);
-	go(s,mid-1,level+1);
-	go(mid+1,e,level+1);
-}
-int main () {
-	cin >> k;
-	int end = pow(2,k) - 1;
-	for(int i = 0 ; i < end ; i++) {
-		cin >> a[i];	
+	go(s,mid-1,level+1); // 중간값을 기준으로 왼쪽값의 미드값체크
+	go(mid+1,e,level+1); // 중간값을 기준으로 오른쪽값의 미드값체크  
+	return ;
+} 
+int main() {
+	cin >> n;
+	int k = pow(2,n) - 1;
+	for(int i = 0 ; i < k ; i ++) {
+		cin >> a[i];
 	}
 	
-	go(0,end,1);
-	for(int i = 1 ; i <= k ; i++) {
+	go(0,k-1,1); // 0 : start , k : end , 1 : level
+	
+	for(int i = 1 ; i <= n ; i++) {
 		for(int j : v[i]) cout << j << ' ';
 		cout << '\n';
 	}
 	return 0;
-} 
+}
