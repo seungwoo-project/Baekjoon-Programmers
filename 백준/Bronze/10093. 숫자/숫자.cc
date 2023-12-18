ce4c990b20db66c0e283d7ade9256d515a37d4a3
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2023-12-18 22:39:41 +0900

    [Bronze II] Title: 숫자, Time: 24 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.cc
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -1,20 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a,b;
+int cnt;
+unsigned long long int a,b;
 int main() {
 	cin >> a >> b;
-	if(a < b) {
-		cout << b-a-1 << '\n';
-		for(int i = a+1 ; i < b; i++) {
-			cout << i << ' ';
+	if(a > b) {
+		cnt = a-b-1;
+		cout << cnt << '\n';
+		for(int i = 0 ; i < cnt ; i++) {
+			cout << ++b << ' ';
 		}
-	} else if (a > b) {
-		cout << a-b-1 << '\n';
-		for(int i = b+1 ; i < a; i++) {
-			cout << i << ' ';
+		
+	} else if(a < b){
+		cnt = b-a-1;
+		cout << cnt << '\n';
+		for(int i = 0 ; i < cnt ; i++) {
+			cout << ++a << ' ';
 		}
+		
 	} else {
-		cout << '0' << '\n';
+		cout << '0';
 	}
 	
 	return 0;
