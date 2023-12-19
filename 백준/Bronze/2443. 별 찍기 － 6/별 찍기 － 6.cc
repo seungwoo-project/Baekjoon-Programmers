2988ba01f4735a401fa35c285d4b10f6b6abde6e
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2023-12-19 14:45:53 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int main() {
+    cin >> n;
+    for(int i = 0 ; i < n ; i++) {
+        for(int j = 0 ; j < i ; j++) {
+            cout << ' ';
+        }
+        for(int k = 2*(n-i)-1 ; k > 0 ; k--) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+    // for(int i = 0 ; i < n ; i++) {
+    //     for(int j = n-i-1 ; j > 0 ; j--) {
+    //         cout << ' ';
+    //     }
+    //     for(int k = 0 ; k < 2*i+1 ; k++) {
+    //         cout << '*';
+    //     }
+    //     cout << '\n';
+    // }
+    
+    return 0;
+}
