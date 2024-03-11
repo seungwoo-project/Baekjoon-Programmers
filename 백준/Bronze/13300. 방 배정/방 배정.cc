3909d933684a3fc8617e6c980bb7e6e0cb9ef8fa
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2024-03-11 23:21:23 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.cc
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[2][6]; // 성별 학년 
+int a[10][2]; // 성별 학년 
 int ret, n, k, s, y;
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -9,16 +9,42 @@ int main() {
 	
 	for(int i = 0 ; i < n ; i++) {
 		cin >> s >> y; // s : 성별 , y : 학년
-		a[s][y]++;
+		a[y][s]++;
 	}
 	
-	for(int i = 0 ; i < 2 ; i++) {
-		for(int j = 0 ; j < 6 ; j++) {
+//	for(int i = 1 ; i <= 6 ; i++) {
+//		for(int j = 0 ; j < 2 ; j++) {
+//			cout << a[i][j] << ' ';
+//		}
+//		cout << '\n';
+//	}
+//	
+	for(int i = 1 ; i <= 6 ; i++) {
+		for(int j = 0 ; j < 2 ; j++) {
 			if(a[i][j] == 1) ret++;
-			else if(a[i][j] % k == 0) ret += a[i][j] / k;
-			else ret = ret + (a[i][j] / k + 1); 
+			else if(a[i][j] % k == 0) ret += a[i][j]/ k;
+			else ret += a[i][j] / k + 1;
 		}
 	}
-	
 	cout << ret;
+
 }
+/*
+16 3
+1 1
+0 1
+1 1
+0 2
+1 2
+0 2
+0 3
+1 3
+1 4
+1 3
+1 3
+0 6
+1 5
+0 5
+1 5
+1 6
+*/
