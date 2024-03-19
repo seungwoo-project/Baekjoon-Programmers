11b3296d917a8005282c5019a7ac47758ec1a214
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2024-03-19 22:29:06 +0900

    [Gold V] Title: 괄호의 값, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.cc
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.cc
@@ -1,45 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-stack<char> s;
-string str;
-
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-
-  cin >> str;
-  int sum = 0; // 누적해서 더해질 값
-  int num = 1; // 곱해질 값
-
-  for(int i = 0; i < str.size(); i++){
-    if(str[i] == '('){
-      num *= 2; // 소괄호가 나오면 곱해질 값 2배 증가
-      s.push(str[i]);
-    }
-    else if(str[i] == '['){
-      num *= 3; // 대괄호가 나오면 곱해질 값 3배 증가
-      s.push(str[i]);
-    }
-    else if(str[i] == ')'){
-      if(s.empty() || s.top() != '('){
-        cout << 0;
-        return 0;
-      }
-      if(str[i-1] == '(') sum += num; // 직전 괄호가 여는 괄호였다면 sum에 값 추가
-      s.pop();
-      num /= 2; // 소괄호 쌍이 사라졌으니 2로 나눔      
-    }
-    else{ // str[i] == ']'
-      if(s.empty() || s.top() != '['){
-        cout << 0;
-        return 0;
-      }
-      if(str[i-1] == '[') sum += num; // 직전 괄호가 여는 괄호였다면 sum에 값 추가
-      s.pop();
-      num /= 3; // 대괄호 쌍이 사라졌으니 3로 나눔
-    }
-  }
-  if(s.empty()) cout << sum;
-  else cout << 0;
+string s;
+stack<char> stk;
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int ret = 0;
+	int a = 1;
+	
+	cin >> s;
+	
+	for(int i = 0 ; i < s.size() ; i++) {
+		if(s[i] == '(') {
+			a *= 2;
+			stk.push(s[i]);
+		} else if(s[i] == '[') {
+			a *= 3;
+			stk.push(s[i]);
+		} else if(s[i] == ')') {
+			if(stk.empty() || stk.top() != '(') {
+				cout << 0;
+				return 0;
+			} 
+			if(s[i-1] == '(') {
+				ret += a;
+			}
+			stk.pop();
+			a /= 2;
+		} else if(s[i] == ']') {
+			if(stk.empty() || stk.top() != '[') {
+				cout << 0;
+				return 0;
+			}
+			if(s[i-1] == '[') {
+				ret += a;
+				
+			}
+			stk.pop();
+			a /= 3;
+		}
+	}
+	
+	if(stk.empty()) cout << ret;
+	else cout << 0;
 }
