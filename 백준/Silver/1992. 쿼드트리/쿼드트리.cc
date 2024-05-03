14606b667f5a00a40b6140e7f8372ba990bd339d
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2024-05-03 16:25:35 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,34 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n;
-int a[68][68];
 
-void go(int sx, int sy, int ex, int ey) {
-	for(int i = sx ; i < ex ; i++) {
-		for(int j = sy ; j < ey ; j++) {
-			if(a[sx][sy] != a[i][j]) {
-				cout << "(";
-				go(sx, sy, (sx+ex)/2, (sy+ey)/2);
-				go(sx, (sy+ey)/2, (sx+ex)/2, ey);
-				go((sx+ex)/2, sy, ex, (sy+ey)/2);
-				go((sx+ex)/2, (sy+ey)/2, ex, ey);
-				cout << ")";
-				return ;
-			}
-		}
-	}
-	cout << a[sx][sy];
+int n;
+char a[65][65];
+
+bool check(int y,int x,int n) {
+
+    for(int i = 0 ; i < n ; i++) {
+        for(int j = 0 ; j < n ; j++) {
+            if(a[y][x] != a[y+i][x+j]) {
+                return 0;
+            }
+        }
+    }
+
+    return 1;
 }
+void go(int y,int x, int n) {
+    // 만약 같은 색상의 색종이가 아니라면 4등분
+
+    if(check(y,x,n)) {
+        cout << a[y][x];
+        return ;
+    } else {
+        // 4등분
+        cout << "(";
+        go(y,x,n/2);
+        go(y,x+n/2,n/2);
+        go(y+n/2,x,n/2);
+        go(y+n/2,x+n/2,n/2);
+        cout << ")";
+    }
+}
+
 int main() {
-	cin >> n;
-	for(int i = 0 ; i < n ; i++) {
-		for(int j = 0 ; j < n ; j++) {
-			scanf("%1d" , &a[i][j]);
-		}
-	}
-	go(0, 0, n, n);
-	
-	
-	
-	return 0;
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> n;
+    for(int i = 0 ; i < n ; i++) {
+        for(int j = 0 ; j < n ; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    go(0,0,n);
+
 }
