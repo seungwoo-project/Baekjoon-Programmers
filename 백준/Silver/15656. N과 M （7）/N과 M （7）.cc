0a5732a08bcd2a034be4d7c32a2bbf87b8463b77
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2024-05-17 09:55:39 +0900

    [Silver III] Title: N과 M (7), Time: 692 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, k;
+int a[10];
+vector<int> v;
+void go(int start) {
+  if (v.size() == k) {
+    for (int i : v) cout << i << " ";
+    cout << '\n';
+    return;
+  }
+
+  for (int i = start + 1; i < n; i++) {
+    v.push_back(a[i]);
+    go(start);
+    v.pop_back();
+  }
+  return;
+}
+
+int main() {
+  cin >> n >> k;
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+
+  sort(a, a + n);
+
+  go(-1);
+  return 0;
+}
