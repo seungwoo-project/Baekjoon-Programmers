872b4b20cce21055370c5cd62b85821befe80b36
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2024-05-26 19:25:58 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,17 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n;
-long long int ret;
+unsigned long long int ret;
 char a;
-int r = 31;
+unsigned long long int r = 1;
 long long int m = 1234567891;
 int main() {
   cin >> n;
   for (int i = 0; i < n; i++) {
     cin >> a;
-    ret += (a - 96) * (int)pow(r, i) % m;
+    ret += (((a - 96) % m) * (r)) % m;
+    r *= 31;
+    r %= m;
   }
 
-  cout << ret;
+  cout << ret % m;
   return 0;
 }
