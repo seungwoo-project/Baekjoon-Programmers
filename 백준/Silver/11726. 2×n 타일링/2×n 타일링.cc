2ffcb2da972048a3eb256de4b8d67e3fe77d57a3
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2024-06-06 15:37:26 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int d[1004];
+int main() {
+  cin >> n;
+  d[1] = 1;
+  d[2] = 2;
+  for(int i = 3 ; i <= n ; i++) {
+    d[i] = (d[i-1] + d[i-2]) % 10007;
+  }
+  
+  cout << d[n];
+  return 0;
+}
