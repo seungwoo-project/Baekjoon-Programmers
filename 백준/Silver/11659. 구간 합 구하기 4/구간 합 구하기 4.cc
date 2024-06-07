6addd59d20fbbe6dee080c9517e2617b27da1e9d
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2024-06-07 20:20:41 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,18 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n,m,a,b;
-int psum[100004], c[100004];
+int s[100004];
+int dp[100004];
+int a,b;
 int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	cin >> n >> m;
-	for(int i = 1 ; i <= n ; i++) {
-		cin >> c[i];
-		psum[i] = psum[i-1] + c[i];
-	}
-	
-	for(int i = 0 ; i < m ; i++) {
-		cin >> a >> b;
-		cout << psum[b] - psum[a-1] << '\n';
-	}
-	return 0;
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  
+  int n,m;
+  cin >> n >> m;
+  for(int i = 1; i <= n ; i++) {
+    cin >> s[i];
+  }
+  
+  for(int i = 1 ; i <= n ; i++) {
+    dp[i] = dp[i-1] + s[i];
+  }
+  
+  for(int i = 0 ; i < m ; i++) {
+    cin >> a >> b;
+    cout << dp[b] - dp[a-1] << '\n';
+  }
+  return 0;
 }
+
+// 점화식 dp[k] = dp[k-1] + s[k];
