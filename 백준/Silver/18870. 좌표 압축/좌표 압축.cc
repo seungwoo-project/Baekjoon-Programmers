4b6156f01f800b7db602f069e45d05377ae8c7ed
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2024-06-26 23:43:13 +0900

    [Silver II] Title: 좌표 압축, Time: 988 ms, Memory: 10036 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 vector<int> a,b;
-map <int,int> mp;
 int main() {
     int n;
     cin >> n;
@@ -13,13 +12,10 @@ int main() {
     b = a;
     sort(a.begin(), a.end());
     a.erase(unique(a.begin(), a.end()), a.end());
-    for(int i = 0 ; i < a.size() ; i++) {
-        mp[a[i]] = lower_bound(a.begin(), a.end(), a[i]) - a.begin();
+    for(int i = 0 ; i < b.size() ; i++) {
+        cout << lower_bound(a.begin(), a.end(), b[i]) - a.begin() << " "; 
     }
     
-    for(int i = 0 ; i < b.size(); i++) {
-        cout << mp[b[i]] << " ";
-    }
 }
 // -10 -9 2 4 4
 // 999 999 999 1000 1000 1000
