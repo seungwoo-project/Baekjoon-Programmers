4cf4a36a7ccb7b7df6de465e753dcbad8e86fe84
leeseungwoo <112483124+seungwoo-project@users.noreply.github.com>
2024-07-08 21:36:18 +0900

    [Gold V] Title: 수 고르기, Time: 20 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.cc
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -1,28 +1,39 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <ios>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
 using namespace std;
 typedef long long ll;
-
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
-    ll ret = 4000000004;
-    ll n, m;
+
+    int n, m;
     cin >> n >> m;
-    ll a[n] = {0,};
+    int a[n] = {0,};
     for(int i = 0 ; i < n ; i++) {
         cin >> a[i];
     }
-    
+
     sort(a, a+n);
-    
-    for(int i = 0 ; i < n ; i++) {
-        ll target = a[i] + m;
-        ll idx = lower_bound(a, a+n, target) - a;
-        if(idx != n) {
-            ret = min(ret, a[idx] - a[i]);
+
+    int start = 0;
+    int end = 0;
+
+    int ret = 2e9;
+    while(end != n) {
+        if(a[end] - a[start] < m) {
+            end++;
+        } else {
+            ret = min(ret, a[end] - a[start]);
+            start++;
         }
     }
-    
+
     cout << ret;
 }
 
-// 1 3 5
+/*
+ * 1 3 5
+ */
